bfa67bfd165e6419ac656f9bd424b001941767e0
Adam Morrison <morrison.adam.s@gmail.com>
2025-08-11 13:34:09 -0500

    Revert "feat: synced date range with compare deltas"

--- web/components/Chart.tsx
+++ web/components/Chart.tsx
@@ -8,29 +8,10 @@ const ReactECharts = dynamic(async () => {
   return import("echarts-for-react");
 }, { ssr: false });
 
-export default function Chart({ option, height = 260, onEvents = {} }: { option: any; height?: number; onEvents?: any }) {
-  const legendHandler = (params: any, chart: any) => {
-    if (params.event?.event?.altKey) {
-      const names = (chart.getOption().series || []).map((s: any) => s.name);
-      const selected: Record<string, boolean> = {};
-      names.forEach((n: string) => {
-        selected[n] = n === params.name;
-      });
-      chart.setOption({ legend: { selected } });
-    }
-  };
-
-  const mergedEvents = {
-    ...onEvents,
-    legendselectchanged: (p: any, c: any) => {
-      legendHandler(p, c);
-      if (onEvents.legendselectchanged) onEvents.legendselectchanged(p, c);
-    },
-  };
-
+export default function Chart({ option, height=260, onEvents }: { option: any; height?: number; onEvents?: any }) {
   return (
     <div className="w-full">
-      <ReactECharts option={option} style={{ height }} notMerge={true} lazyUpdate={true} onEvents={mergedEvents} />
+      <ReactECharts option={option} style={{ height }} notMerge={true} lazyUpdate={true} onEvents={onEvents} />
     </div>
   );
 }
